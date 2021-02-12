610bed61ecb66d41220333905b1b19917b7f9c1d
Virej Dasani <dasanivirej@gmail.com>
2021-02-12 20:11:55 +0530

    Added autoCommit.py and commitCommand.txt

---
+++ autoCommit.py
@@ -0,0 +1,22 @@
+import os
+
+ip = int(input("How many times do you want to commit? \n"))
+autoPush = input("Auto git push when commited? (y/n) \n")
+
+for i in range(ip):
+	os.system('git commit --allow-empty -m "Trigger notification $(date)"')
+
+if autoPush == "y":
+	os.system('git push')
+	
+# git commit --allow-empty -m "Trigger notification $(date)"
+
+#  MADE BY:_            _   _____                        _ 
+#  \ \    / (_)        (_) |  __ \                      (_)
+#   \ \  / / _ _ __ ___ _  | |  | | __ _ ___  __ _ _ __  _ 
+#    \ \/ / | | '__/ _ \ | | |  | |/ _` / __|/ _` | '_ \| |
+#     \  /  | | | |  __/ | | |__| | (_| \__ \ (_| | | | | |
+#      \/   |_|_|  \___| | |_____/ \__,_|___/\__,_|_| |_|_|
+#                     _/ |                                 
+#                    |__/                                  
+
