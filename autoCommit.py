b9c425677effbe054681316c11a06943ad49a631
Virej Dasani <dasanivirej@gmail.com>
2021-03-07 14:51:08 +0530

    Changed commit message in autoCommit.py

--- autoCommit.py
+++ autoCommit.py
@@ -4,7 +4,8 @@ ip = int(input("How many times do you want to commit? \n"))
 autoPush = input("Auto git push when commited? (y/n) \n")
 
 for i in range(ip):
-	os.system('git commit --allow-empty -m "New Commit at: $(date)"')
+	# os.system('git commit --allow-empty -m "New Commit at: $(date)"')
+	os.system(f'git commit --allow-empty -m "Commit {i} of {ip}"')	
 
 print("Commited " + str(ip) + " times")
 
